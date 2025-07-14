8c4191ddcef049cf138c8cea92e466d4cace00c5
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-07-14 19:04:45 +0000

    app.config.ts

--- angul-it/src/app/app.config.ts
+++ angul-it/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
-};
+export const appConfig = () => [
+  provideRouter(routes),
+  provideHttpClient(),
+  // Ajoute ici d'autres providers globaux si besoin
+];
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "
-app.routes.ts
+app.config.ts
 "
 git push gitea
 git push github
